94a3a31a3112e21b8c296ec00ca66f1b7291fb5d
Swidnixx <swidnixx@gmail.com>
2022-10-11 17:03:54 +0200

    Kontroler Gracza

---
+++ Assets/Scripts/LookAround.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    [SerializeField] float sensivity = 500;
+    Transform player;
+    float xRot;
+
+    void Start()
+    {
+        player = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+
+        player.Rotate(new Vector3(0, mouseX, 0));
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -70, 50);
+        transform.localRotation = Quaternion.Euler(new Vector3(xRot, 0, 0));
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 10;
+    CharacterController controller;
+    float velocityY;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Movement();
+        GroundSensor();
+    }
+
+    void Movement()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Debug.Log(new Vector2(inputX, inputY));
+
+        Vector3 movement = inputX * transform.right + inputY * transform.forward;
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+    void GroundSensor()
+    {
+        RaycastHit hit;
+        bool didHit = Physics.Raycast(transform.position + Vector3.down, Vector3.down, out hit, 0.5f);
+
+        if(didHit)
+        {
+            velocityY = 0;
+        }
+        else
+        {
+            Gravity();
+        }
+    }
+
+    void Gravity()
+    {
+        if (velocityY < 55)
+        {
+            velocityY += 10 * Time.deltaTime; 
+        }
+
+        controller.Move( Vector3.down * velocityY * Time.deltaTime );
+    }
+}
